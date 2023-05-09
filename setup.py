9804265f6de10667ea921628f14158c0cf79072a
Shishuai Hu <sshu@mail.nwpu.edu.cn>
2023-05-09 14:34:39 +0800

    init: evaluator

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_namespace_packages
+
+setup(name='mevaluator',
+      packages=find_namespace_packages(include=["mevaluator", "mevaluator.*"]),
+      version='0.1',
+      description='MEvaluator: evaluator for medical image segmentation.',
+      url='https://github.com/ShishuaiHu/MEvaluator',
+      author='',
+      author_email='',
+      license='Apache License Version 2.0, January 2004',
+      python_requires=">=3.9",
+      install_requires=[
+          "medpy",
+          "batchgenerators>=0.25",
+          "numpy",
+          "SimpleITK>=2.2.1",
+          "pandas",
+      ],
+      entry_points={
+          'console_scripts': [
+              'evaluate_folder = mevaluator.evaluator:nnunet_evaluate_folder'  # api available
+          ],
+      },
+      keywords=['deep learning', 'image segmentation', 'medical image analysis',
+                'medical image segmentation']
+      )
